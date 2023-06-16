89db55feb935fda6ce86f808bf885240b0969b8e
Alex Micharski <amicharski@outlook.com>
2023-06-16 00:53:06 -0400

    started working on printing full team rosters; temporarily disabled multiplier

---
+++ build/CMakeFiles/footballgame.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alex/footballsim/coach.c" "CMakeFiles/footballgame.dir/coach.c.o" "gcc" "CMakeFiles/footballgame.dir/coach.c.o.d"
+  "/home/alex/footballsim/csv_parser.c" "CMakeFiles/footballgame.dir/csv_parser.c.o" "gcc" "CMakeFiles/footballgame.dir/csv_parser.c.o.d"
+  "/home/alex/footballsim/draft.c" "CMakeFiles/footballgame.dir/draft.c.o" "gcc" "CMakeFiles/footballgame.dir/draft.c.o.d"
+  "/home/alex/footballsim/game.c" "CMakeFiles/footballgame.dir/game.c.o" "gcc" "CMakeFiles/footballgame.dir/game.c.o.d"
+  "/home/alex/footballsim/league.c" "CMakeFiles/footballgame.dir/league.c.o" "gcc" "CMakeFiles/footballgame.dir/league.c.o.d"
+  "/home/alex/footballsim/main.c" "CMakeFiles/footballgame.dir/main.c.o" "gcc" "CMakeFiles/footballgame.dir/main.c.o.d"
+  "/home/alex/footballsim/main_menu.c" "CMakeFiles/footballgame.dir/main_menu.c.o" "gcc" "CMakeFiles/footballgame.dir/main_menu.c.o.d"
+  "/home/alex/footballsim/player.c" "CMakeFiles/footballgame.dir/player.c.o" "gcc" "CMakeFiles/footballgame.dir/player.c.o.d"
+  "/home/alex/footballsim/team.c" "CMakeFiles/footballgame.dir/team.c.o" "gcc" "CMakeFiles/footballgame.dir/team.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
